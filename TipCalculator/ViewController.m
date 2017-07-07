5152ddc2a2fbefb6aa58b82cd2fc4b8114cf1698
Shreve-La <shrevela@gmail.com>
2017-07-07 13:07:14 -0400

    default 15% tip and custom tip implemented

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *customTipTextField;
+
 @end
 
 @implementation ViewController
@@ -23,6 +27,32 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)defaultTip{
+    [self calcTipWith:15];
+}
+- (IBAction)CalcDefaultPress:(id)sender {
+    [self calcTipWith:15];
+
+}
+
+-(void)calcTipWith:(float)tipAmount{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+    
+    float billAmount = [numberFormatter numberFromString:self.billAmountTextField.text].floatValue;
+    float result = billAmount * tipAmount / 100;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"%.02f", result];
+}
+
+- (IBAction)calcCustomTip:(id)sender {
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+     float tipAmount = [numberFormatter numberFromString:self.customTipTextField.text].floatValue;
+    
+    [self calcTipWith:tipAmount];
+    
 }
 
 
